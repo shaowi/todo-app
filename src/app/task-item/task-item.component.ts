5be2ee34623913594f8e49def9515e465cfab904
shaowi <lee_shao_wee@hotmail.com>
2024-06-22 14:21:03 +0800

    chore: Update task item component to include input field and delete button

--- src/app/task-item/task-item.component.ts
+++ src/app/task-item/task-item.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../task.service';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.sass'
+  styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent {
+  @Input() task!: Task;
+  @Output() update = new EventEmitter<Task>();
+  @Output() delete = new EventEmitter<number>();
 
+  editMode = false;
+  editedTitle = '';
+
+  toggleCompleted(): void {
+    this.task.completed = !this.task.completed;
+    this.update.emit(this.task);
+  }
+
+  enterEditMode(): void {
+    this.editMode = true;
+    this.editedTitle = this.task.title;
+  }
+
+  saveEdit(): void {
+    if (this.editedTitle.trim()) {
+      this.task.title = this.editedTitle;
+      this.update.emit(this.task);
+      this.editMode = false;
+    }
+  }
+
+  cancelEdit(): void {
+    this.editMode = false;
+  }
+
+  deleteTask(): void {
+    this.delete.emit(this.task.id);
+  }
 }
