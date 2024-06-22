335612df53fb1e38632bfbface38b04ac98d6baa
shaowi <lee_shao_wee@hotmail.com>
2024-06-22 15:06:52 +0800

    chore: Update task item and task list components' stylesheets to use Sass

--- src/app/task-item/task-item.component.ts
+++ src/app/task-item/task-item.component.ts
@@ -4,7 +4,7 @@ import { Task } from '../task.service';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.sass'],
+  styleUrls: ['./task-item.component.scss'],
 })
 export class TaskItemComponent {
   @Input() task!: Task;
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -4,7 +4,7 @@ import { Task, TaskService } from '../task.service';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.sass'],
+  styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent implements OnInit {
   tasks: Task[] = [];
